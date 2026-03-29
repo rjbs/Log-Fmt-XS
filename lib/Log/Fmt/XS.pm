3b317b1f3318a5f52bc48799a3ccc756761ded34
Ricardo Signes <rjbs@semiotic.systems>
2026-03-29 19:03:05 -0400

    do not monkeypatch Log::Fmt
    …instead, just be there.  Log::Fmt will learn to use Log::Fmt::XS on its
    own.

--- lib/Log/Fmt/XS.pm
+++ lib/Log/Fmt/XS.pm
@@ -5,11 +5,4 @@ package Log::Fmt::XS 0.001;
 use XSLoader;
 XSLoader::load('Log::Fmt::XS', $Log::Fmt::XS::VERSION);
 
-sub import {
-    require Log::Fmt;
-    no warnings 'redefine';
-    *Log::Fmt::_pairs_to_kvstr_aref = \&_pairs_to_kvstr_aref;
-    *Log::Fmt::_quote_string = \&_quote_string;
-}
-
 1;
