ed57409b7b513cdbc414db5dd3f68b971e1f7b5a
Ricardo Signes <rjbs@semiotic.systems>
2026-03-31 15:22:50 -0400

    Log::Fmt::XS: add some docs

--- lib/Log/Fmt/XS.pm
+++ lib/Log/Fmt/XS.pm
@@ -2,6 +2,18 @@ use v5.20;
 use warnings;
 package Log::Fmt::XS 0.001;
 
+# ABSTRACT: XS components to speed up Log::Fmt
+
+=head1 OVERVIEW
+
+There isn't much to say!  If you have Log::Fmt::XS installed, and a Log::Fmt
+new enough to use it, the XS component will be used to make emitting logfmt
+logs faster.
+
+See L<Log::Fmt> for more information.
+
+=cut
+
 use XSLoader;
 XSLoader::load('Log::Fmt::XS', $Log::Fmt::XS::VERSION);
 
